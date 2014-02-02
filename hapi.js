bb13ba81e3a7d0b79bb36aeeb5b206b7a4244fcb
Jim Heising <jheising@redfoundry.com>
2014-02-01 21:41:34 -0800

    Stuff

---
+++ hapi.js
@@ -0,0 +1,53 @@
+var hapi = {};
+
+hapi.HAPI_VERB = {
+    get: "getting",
+    create: "creating",
+    update: "changing",
+    delete: "deleting"
+};
+
+hapi.makeHAPISuccess = function (HAPI_VERB, resourceType, data) {
+    var hapiResponse = {
+        "this": "succeeded",
+        "by": HAPI_VERB,
+        "the": resourceType,
+        "with": data
+    };
+
+    return hapiResponse;
+}
+
+hapi.makeHAPIFailure = function (why, httpErrorCode, apiErrorCode) {
+    httpErrorCode = httpErrorCode || 500;
+
+    var hapiResponse = {
+        "this": "failed",
+        "with": apiErrorCode || httpErrorCode,
+        "because": why
+    };
+
+    return hapiResponse;
+}
+
+hapi.middleware = function (req, res, next) {
+
+    res.sendHAPISuccess = function(HAPI_VERB, resourceType, data)
+    {
+        res.send(hapi.makeHAPISuccess(HAPI_VERB, resourceType, data));
+    }
+
+    res.sendHAPIFailure = function(why, httpErrorCode, apiErrorCode)
+    {
+        res.send(hapi.makeHAPIFailure(why, httpErrorCode, apiErrorCode));
+    }
+
+    res.sendHAPINotFound = function()
+    {
+        res.sendHAPIFailure("we couldn't find this", 404, 404);
+    }
+
+    return next();
+};
+
+module.exports.hapi = hapi;
